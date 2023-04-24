afcc83f7680e72713f53750803c4d5b139a5f403
Alex <alex.fiendish@gmail.com>
2023-04-24 19:52:12 +0300

    added basic project structure, styles, webpack config

---
+++ src/assets/scripts/MyPlace.js
@@ -0,0 +1,5 @@
+import '../libs/normalize.css';
+import '../styles/_fonts.scss';
+import '../styles/_app.css';
+import '../styles/_my-place.css';
+export const myPlace = async (a, b) => a * b;
---
+++ src/assets/scripts/SharePlace.js
@@ -0,0 +1,5 @@
+import '../libs/normalize.css';
+import '../styles/_fonts.scss';
+import '../styles/_app.css';
+import '../styles/_share-place.css';
+export const sharePlace = 'Share place module';
