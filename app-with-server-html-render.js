97224071095d51915d91ac35953da5f78d9c42f5
Alex <alex.fiendish@gmail.com>
2023-04-28 19:29:27 +0300

    added code examples: work with file system, how to create server with serverside html rendering, basic server REST API, and connection to MongoDB

---
+++ app-with-server-html-render.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use((req, res, next) => {
+  res.setHeader('Content-Type', 'text/html');
+  next();
+});
+
+app.use((req, res, next) => {
+  const userName = req.body.username || 'Unknown User';
+
+  res.render('index', {
+    user: userName,
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Listening on port 3000...');
+});
---
+++ routes/location.js
@@ -0,0 +1,85 @@
+const express = require('express');
+const { MongoClient, ObjectId } = require('mongodb');
+
+const router = express.Router();
+const url =
+  'mongodb+srv://user:hYU50Ruu7r7a1xun@cluster0.jbcie.mongodb.net/locationsJSCompleteGuide';
+const client = new MongoClient(url);
+
+// In memory storage
+// const locationStorage = {
+//   locations: [
+//     { id: 1, address: 'Somewhere on Earth', coords: { lat: 123, lng: 321 } },
+//   ],
+// };
+
+router.post('/add-location', (req, res, next) => {
+  // const id = Math.random();
+
+  async function main() {
+    // Use connect method to connect to the server
+    await client.connect();
+
+    const db = client.db('locationsJSCompleteGuide');
+    const collection = db.collection('user-locations');
+    const result = await collection.insertOne({
+      address: req.body.address,
+      coords: { lat: req.body.lat, lng: req.body.lng },
+    });
+
+    res.json({
+      message: 'Stored location!',
+      locId: result.insertedId,
+    });
+
+    return 'done.';
+  }
+
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+
+  // locationStorage.locations.push({
+  //   id,
+  //   address: req.body.address,
+  //   coords: { lat: req.body.lat, lng: req.body.lng },
+  // });
+  // console.log(locationStorage);
+});
+
+router.get('/location/:lid', (req, res, next) => {
+  const locationId = req.params.lid;
+
+  async function main() {
+    await client.connect();
+
+    const db = client.db('locationsJSCompleteGuide');
+    const collection = db.collection('user-locations');
+
+    const filteredLocationResult = await collection.findOne({
+      _id: new ObjectId(locationId),
+    });
+
+    if (!filteredLocationResult) {
+      return res.status(404).json({ message: 'Not found!' });
+    }
+    res.json({
+      address: filteredLocationResult.address,
+      coordinates: filteredLocationResult.coords,
+    });
+
+    return 'done.';
+  }
+
+  main()
+    .then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
+
+  // const location = locationStorage.locations.find((loc) => {
+  //   return loc.id === locationId;
+  // });
+});
+
+module.exports = router;
