f26397f572e275b2d91add328c3d0139304eda80
Alex <alex.fiendish@gmail.com>
2023-04-28 21:16:07 +0300

    added port env

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ app.use((req, res, next) => {
 });
 
 app.use(locationRoutes);
-
-app.listen(3000, () => {
-  console.log('Listening on port 3000...');
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Listening on port ${PORT}...`);
 });
